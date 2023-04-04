e0ce31043b7b123bd2ccc46b8581fc7a53eb7761
Kerman-ap <87071358+Kerman-ap@users.noreply.github.com>
2023-04-04 23:44:51 +1000

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ const element = document.getElementById('time');
 setInterval(function () {
   const currentDate = new Date();
   element.innerText = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
-}, 16);
+}, 1000);
